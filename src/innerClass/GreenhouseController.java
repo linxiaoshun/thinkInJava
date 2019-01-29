e6c1013caeaf29e238705eceb7a402f678dc9013
vestgirl <1739313742@qq.com>
2019-01-29 16:50:50 +0800

    内部类与控制框架

---
+++ src/innerClass/GreenhouseController.java
@@ -0,0 +1,31 @@
+package innerClass;
+
+import innerClass.controller.Event;
+
+/**
+ * @Auther: Gon
+ * @Date: 2019/1/29 16:09
+ * @Description:
+ */
+public class GreenhouseController {
+    public static void main(String[] args) {
+        GreenhouseControls gc = new GreenhouseControls();
+        // Instead of hard-wiring, you could parse
+        // configuration information from a text file here:
+        gc.addEvent(gc.new Bell(900));
+        Event[] eventList = {
+                gc.new ThermostatNight(0),
+                gc.new LightOn(200),
+                gc.new LightOff(400),
+                gc.new WaterOn(600),
+                gc.new WaterOff(800),
+                gc.new ThermostatDay(1400)
+        };
+        gc.addEvent(gc.new Restart(2000, eventList));
+        if(args.length == 1)
+            gc.addEvent(
+                    new GreenhouseControls.Terminate(
+                            new Integer(args[0])));
+        gc.run();
+    }
+}
